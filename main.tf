9e8f33588c15e459dd45c4730ab4f9d5c3c1edc6
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-13 09:57:21 +0200

    testing with existing vpcs

--- main.tf
+++ main.tf
@@ -1,32 +1,5 @@
-
-module "vpc_a" {
-  source             = "github.com/sibabalwe47/vpc"
-  name               = "vpc-a"
-  region             = "us-east-1"
-  vpc_cidr           = "10.0.0.0/16"
-  enable_nat_gateway = false
-  single_nat_gateway = false
-  tags = {
-    Name = "vpc-a"
-  }
-}
-
-module "vpc_b" {
-  source             = "github.com/sibabalwe47/vpc"
-  name               = "vpc-b"
-  region             = "us-east-1"
-  vpc_cidr           = "172.0.0.0/16"
-  enable_nat_gateway = false
-  single_nat_gateway = false
-  tags = {
-    Name = "vpc-b"
-  }
-}
-
-
 module "vpc_peering" {
-  depends_on  = [module.vpc_a, module.vpc_b]
   source      = "./src"
-  peer_vpc_id = module.vpc_a.vpc_id
-  vpc_id      = module.vpc_b.vpc_id
+  peer_vpc_id = var.peer_vpc_id
+  vpc_id      = var.vpc_id
 }
---
+++ values.tfvars
@@ -0,0 +1,4 @@
+vpc_id      = "vpc-06e424dd06bd7a52c"
+peer_vpc_id = "vpc-0ec8f1e32edbca33a"
+
+# variable "tags" {}
