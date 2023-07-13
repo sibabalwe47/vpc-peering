95af8fab4ed30ab0bf5aa26f9a0a464f5f2c3b05
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-13 07:13:14 +0200

    Added resources

---
+++ data.tf
@@ -0,0 +1,17 @@
+
+data "aws_vpc" "accepter" {
+  id = var.peer_vpc_id
+}
+
+data "aws_route_tables" "accepter" {
+  vpc_id = data.aws_vpc.accepter.id
+}
+data "aws_vpc" "owner" {
+  id = var.vpc_id
+}
+
+data "aws_route_tables" "owner" {
+  vpc_id = var.vpc_id
+}
+
+
