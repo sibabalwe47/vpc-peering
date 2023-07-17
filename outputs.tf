84b88a60558a80034b986092562a49ccc2da5666
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-17 07:39:01 +0200

    corrected output references

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 
 output "aws_vpc_peering_connection_id" {
-  value = module.vpc_peering.id
+  value = module.vpc_peering.aws_vpc_peering_connection_id
 }
 
 output "accepter_status" {
-  value = module.vpc_peering.id
+  value = module.vpc_peering.accepter_status
 }
